9235b2d1985756c612e47266b1d593ef53eee02b
Zhivko <zivkostoimcev1551@outlook.com>
2025-10-21 23:12:52 +0200

    ZTR-#2 started configuring main function with DI?

--- src/main/java/dev/Main.java
+++ src/main/java/dev/Main.java
@@ -1,17 +1,35 @@
 package dev;
 
 import dev.network.NetworkManager;
-import dev.utils.Logger;
+import dev.utils.Config;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-
-    public Main() {
-        NetworkManager networkManager = new NetworkManager();
-    }
+    private static NetworkManager networkManager;
+    private static ExecutorService executorService;
+    private static Config config;
 
     public static void main(String[] args) {
+        networkManager = new NetworkManager();
+        executorService = Executors.newVirtualThreadPerTaskExecutor();
+        config = Config.load(args[0]);
 
-        Logger logger = new Logger(Main.class);
-        logger.info("Application started.");
+        startNetwork();
+    }
+
+    private static void startNetwork() {
+        // one thread starts the server socket to listen for incoming connections
+        // another thread manages outgoing connections
+        // for now, just start the server socket
+
+        // if the node is bootstrap node, it doesn't need to connect to other nodes
+        if (config.isBootstrapNode()) {
+            // log that we are bootstrap node and start accepting connections
+        }
+
+
+
     }
 }
